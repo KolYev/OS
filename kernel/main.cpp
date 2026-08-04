0091262ebaa5d9d2f62142861ec6990faec0bd8c
KolYev <evgeniy,kolesenko@gmail.com>
2026-08-04 12:09:19 +0300

    Ввод символом с алфавитно-цифрового блока клавиатуры

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -29,6 +29,17 @@ namespace
 extern "C" [[noreturn]] void KernelMain()
 {
     constexpr uint8_t color = 0x0F;
+    constexpr char Keyboard[] = {
+        'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']',
+        'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';',
+        'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/'
+    };
+    constexpr uint8_t scancodes[] = {
+        0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B,
+        0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
+        0x2C, 0x2D, 0x2E, 0x2F, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35
+    };
+
     volatile auto* const vga = reinterpret_cast<volatile uint16_t*>(0xB8000);
 
     for (uint32_t index = 0; index < sizeof(VgaMessage) - 1; ++index)
@@ -38,6 +49,7 @@ extern "C" [[noreturn]] void KernelMain()
 
     SerialInit();
     uint32_t vga_index = 80;
+    uint8_t keyboard_size = sizeof(Keyboard) / sizeof(Keyboard[0]);
 
     for (;;)
     {
@@ -46,8 +58,12 @@ extern "C" [[noreturn]] void KernelMain()
         if (!(scancode & 0x80))
         {
             char character = '?';
-            if (scancode == 0x1E) character = 'A';
-            else if (scancode == 0x30) character = 'B';
+            
+            for (uint8_t i = 0; i < keyboard_size; ++i)
+            {
+                if (scancode == scancodes[i]) character = Keyboard[i];
+            }
+            
 
             if (vga_index < 80 * 25)
             {
